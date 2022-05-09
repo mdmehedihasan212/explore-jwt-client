c483afc0b46287df2ffad88b0c8279334399b3dc
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-09 17:34:34 +0600

    add fetch api from server and create login jot

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
 const Login = () => {
+
+    const handleToSubmit = event => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        fetch('http://localhost:5000/login', {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <>
+            <h1 className='text-center my-3'>Login</h1>
+            <form onSubmit={handleToSubmit} className='mx-auto py-2' style={{ width: '500px' }}>
+                <div className="mb-3">
+                    <input name='email' placeholder='Email' type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div className="mb-3">
+                    <input name='password' placeholder='Password' type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+                <button type="submit" className="btn btn-primary w-50 mx-auto">Submit</button>
+            </form>
+        </>
     );
 };
 
